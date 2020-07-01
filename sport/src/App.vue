0f5b86a8c8753e8ed6896f63a4dc3b86da59125d
liuxiaokun <1844861420@qq.com>
2020-07-02 00:17:43 +0800

    test

--- sport/src/App.vue
+++ sport/src/App.vue
@@ -1,60 +1,28 @@
 <template>
   <div id="app">
-    <transition name="fade" mode="out-in">
-      <router-view></router-view>
-    </transition>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    components: {}
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
   }
+}
 </script>
 
-<style lang="scss">
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
-    font-size: 14px;
-    -webkit-font-smoothing: antialiased;
-  }
-
-  #app {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    width: 100%;
-  }
-
-  .el-submenu [class^=fa] {
-    vertical-align: baseline;
-    margin-right: 10px;
-  }
-
-  .el-menu-item [class^=fa] {
-    vertical-align: baseline;
-    margin-right: 10px;
-  }
-
-  .toolbar {
-    background: #f2f2f2;
-    padding: 10px;
-    margin: 10px 0;
-    .el-form-item {
-      margin-bottom: 10px;
-    }
-  }
-
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: all 0.2s ease;
-  }
-
-  .fade-enter,
-  .fade-leave-active {
-    opacity: 0;
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
