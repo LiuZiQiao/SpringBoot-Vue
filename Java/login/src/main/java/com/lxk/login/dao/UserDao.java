2a2e16d2e928ccc27ccfb4797d017d4a7829845d
liuziqiao <1844861420@qq.com>
2020-07-28 21:32:51 +0800

    更新注册登录

--- Java/login/src/main/java/com/lxk/login/dao/UserDao.java
+++ Java/login/src/main/java/com/lxk/login/dao/UserDao.java
@@ -19,7 +19,6 @@ public interface UserDao {
 //    @Select("select *from user where username=#{username} and password=#{password}")
     public User login(@Param("username") String username,@Param("password") String password);
 
-
     boolean register(User user);
 
 //    @Select("SELECT COUNT(*) from user")
